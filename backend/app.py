54f138a20e4437ebc8e1ed74fa5f416de6801a1a
Tahaa Farooq <tahacodez@gmail.com>
2022-11-26 13:32:26 +0300

    Created Database Model

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,58 @@
 from flask import Flask, request, jsonify, abort
+from model import Users, Products, ProductRequest, db
+from os import urandom
+
+app = Flask(__name__)
+app.secret_key = urandom(12)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+
+DATABASE = {
+    'drivername': 'sqlite',
+    'database': 'database.db'
+}
+
+db.init_app(app)
+
+
+# creating database tables
+@app.before_first_request()
+def create_tables():
+    db.create_all()
+
+
+# registration route
+@app.route("/registration", methods=["POST"])
+def registration_route():
+    if request.method == "POST":
+        data = request.get_json()
+        email = data['email']
+        password = Users.set_password(data['password'])
+        location = data['location']
+        role = data['role']
+        phone_number = data['phone_number']
+
+        check_user = Users.query.filter_by(email=email).first()
+
+        if check_user is not None:
+            return jsonify({"status": False, "message": "User Exists"})
+        else:
+            reg_user = Users(email, password, location, role, phone_number)
+            db.session.add(reg_user)
+            db.commit()
+
+            return jsonify({"status": True, "message": "User Registered"})
+
+
+# login route
+@app.route("/login", methods=["POST"])
+def login_route():
+    if request.method == "POST":
+        data = request.get_json()
+        email = data["email"]
+        password = data["password"]
+
+        pass
+
+
